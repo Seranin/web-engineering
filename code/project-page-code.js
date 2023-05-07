c59efbbb2aeb652dedcdbd9dc9963f24df5e64fd
Seranin <auchoban@gmail.com>
2023-05-07 11:55:38 +0200

    Css + validation left

---
+++ code/project-page-code.js
@@ -0,0 +1,40 @@
+var max = 6
+$(document).ready(function(){
+    var i = 0;
+    $("#previous-button").on("click", function(){
+        $(`#img${i}`).removeClass("active");
+        $(`#des${i}`).removeClass("active");
+        if ( i <= 0)
+        {
+            i = max;
+        } else
+        {
+            i = i - 1;
+        }
+        $(`#img${i}`).addClass("active");
+        $(`#des${i}`).addClass("active");
+    });
+    $("#next-button").on("click", function(){
+        $(`#img${i}`).removeClass("active");
+        $(`#des${i}`).removeClass("active");
+        if ( i >= max)
+        {
+            i = 0;
+        } else
+        {
+            i = i + 1;
+        }
+        $(`#img${i}`).addClass("active");
+        $(`#des${i}`).addClass("active");
+    });
+    $("#block").on("click", function(){
+        if ($("#imgblock").hasClass("show")){
+            $("#imgblock").removeClass("show");
+        } else
+        {
+            $("#imgblock").addClass("show")
+        }
+    });
+});
+
+
